9d452ca7fddae3a4db2357b1516b5bbb1f432b3c
Yryskeldi <kuralyrys@gmail.com>
2024-05-11 22:55:35 +0300

    connect background-api for trustUs

--- src/shared/index.js
+++ src/shared/index.js
@@ -2,7 +2,6 @@ export { ImageModal } from './ui/imageModal/ImageModal';
 export { Button } from './ui/button/Button';
 export { Advertising } from './ui/advertising/Advertising';
 export { useMediaQuery } from './hooks/useMediaQuery';
-export { AnimationTitle } from './ui/animationTitle/AnimationTitle';
 export { scrollToTop } from './utils/scrollToTop';
 export { CardSectionGroup } from './ui/cardSectionGroup/CardSectionGroup';
 export { CardRehearsalSection } from './ui/cardRehearsalSection/CardRehearsalSection';
