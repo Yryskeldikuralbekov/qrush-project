a377084516724cf35875dca854ba84aca77fe91a
Sultanbek <sultan.02173@gmail.com>
2024-05-16 23:56:41 +0600

    fix bugs error message in faq page and redesign buttons

--- src/features/formFeedback/FormFeedback.jsx
+++ src/features/formFeedback/FormFeedback.jsx
@@ -40,7 +40,7 @@ export const FormFeedback = ({ textArr }) => {
   };
   return (
     <section className='mx-[20px] sm:mx-[auto] pb-[70px] lg:pb-[150px] xl:max-w-[1300px] lg:max-w-[900px] text-[#FFFFFF]'>
-      <h2 className='text-left w-[195px] pb-[23px] text-[20px] sm:w-full sm:text-center sm:pb-[90px] sm:text-[24px] xl:text-[50px] lg:text-[30px]'>
+      <h2 className='pb-[23px] text-[20px] sm:w-full text-center sm:pb-[24px] lg:pb-[80px] sm:text-[24px] xl:text-[50px] lg:text-[30px]'>
         {textArr.title}
       </h2>
 
@@ -205,7 +205,7 @@ export const FormFeedback = ({ textArr }) => {
               </div>
             </div>
             <label htmlFor='checkbox' className='cursor-pointer'>
-              <p className='text-[14px] font-[600] xl:text-[14px] md:text-[12px]'>
+              <p className='text-[14px] font-[600] xl:text-[14px] md:text-[12px] tablet:w-[300px]'>
                 {textArr.policyPrivacy}
               </p>
             </label>
